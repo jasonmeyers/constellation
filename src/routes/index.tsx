944ab794267f236eaf5b5585b3fce686bba39712
Jason Meyers <jason.meyers@gmail.com>
2026-01-13 15:58:14 -0800

    install done and tested

---
+++ src/routes/index.tsx
@@ -0,0 +1,13 @@
+import { createFileRoute } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/')({
+  component: Index,
+})
+
+function Index() {
+  return (
+    <div className="p-2">
+      <h3 className="text-3xl font-bold">Welcome Home!</h3>
+    </div>
+  )
+}
